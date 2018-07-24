09d282dfbf6e25958dfd6bd031c55d4f5857e605
ganesh <gpagar641@gmail.com>
2018-07-24 10:58:13 +0530

    //Ganesh

---
+++ PH0100FrontEnd/src/main/java/com/gensoft/ph/product/controller/ProductDetailsController.java
@@ -0,0 +1,23 @@
+package com.gensoft.ph.product.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ProductDetailsController {
+
+	
+	
+	@RequestMapping(value = "/showSingleProductDetails", method = RequestMethod.GET)
+	public ModelAndView showSingleProductDetails(HttpServletRequest req, HttpServletResponse res )
+	{
+		ModelAndView model=new ModelAndView("single_product_details");
+		
+		return model;
+	}
+}
